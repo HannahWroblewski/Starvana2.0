c8660248df054cfd00cd44f7253cd1838c43a596
HannahWroblewski <hannah.wrob@gmail.com>
2022-06-04 17:44:14 -0400

    checkout page almost done

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCx0tcar2EmnD2tGQ3CjNDy3-Tf363YiPM",
+    authDomain: "starvana2-9d9db.firebaseapp.com",
+    projectId: "starvana2-9d9db",
+    storageBucket: "starvana2-9d9db.appspot.com",
+    messagingSenderId: "508991125201",
+    appId: "1:508991125201:web:8f4b5c7fbf718fb4db77f0",
+    measurementId: "G-7S7H73K4HL"
+  };
