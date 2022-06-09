65903dc19fc3f28ff80c44cca24126103a98ef16
HannahWroblewski <hannah.wrob@gmail.com>
2022-06-09 16:06:56 -0400

    checkout page completed

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./Checkout.css";
 import Subtotal from "./Subtotal";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
 
-const Checkout = () => {
+function Checkout() {
+  const [{ basket, user }, dispatch] = useStateValue();
+
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -13,8 +17,19 @@ const Checkout = () => {
         />
 
         <div>
-          <h2  className="checkout__title">Your shopping basket</h2>
-          {/* {BasketItem} */}
+          <h3>Hello, {user?.email}</h3>
+          <h2 className="checkout__title">Your shopping Basket</h2>
+
+          {basket.map(item => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
+
         </div>
       </div>
 
@@ -23,6 +38,6 @@ const Checkout = () => {
       </div>
     </div>
   );
-};
+}
 
 export default Checkout;
